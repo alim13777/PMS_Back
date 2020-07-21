5278e26d76026177fd02e2a0cf9b8ac0d392c080
alireza <alim13777@gmail.com>
2020-07-21 11:22:00 +0430

    initializing sanctum

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Book;
+use App\User;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +12,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+
     }
 }
