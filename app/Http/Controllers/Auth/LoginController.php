ab8f0ac73b483c50afaf15239308099af248a492
alireza <alim13777@gmail.com>
2020-07-24 17:15:51 +0430

    login response

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,11 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+    protected function authenticated(Request $request, $user)
+    {
+        $data=$user->party->person()->get()[0];
+        $data=array("firstName"=>$data["firstName"],"lastName"=>$data["lastName"],"userName"=>$user["name"]);
+        return response()->json($data);
     }
 }
