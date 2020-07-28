19b6f63cec27ecb37f2627e15c78f1f332b13c3a
alireza <alim13777@gmail.com>
2020-07-28 13:08:25 +0430

    email verify

--- app/Notifications/UserVerification.php
+++ app/Notifications/UserVerification.php
@@ -74,14 +74,8 @@ class UserVerification extends Notification
    */
     protected function verificationUrl($notifiable)
     {
-        return URL::temporarySignedRoute(
-            'verification.verify',
-            Carbon::now()->addMinutes(
-                Config::get('auth.verification.expire', 60)),
-            [
-                'id' => $notifiable->getKey(),
-                'hash' => sha1($notifiable->getEmailForVerification()),
-            ]
-        );
+
+        return "localhost:3000/email/verify";
+
     }
 }
